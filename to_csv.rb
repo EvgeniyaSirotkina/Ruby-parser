795b3d22a4ae7967c6587683feb5c14eb0584207
EvgeniyaSirotkina <47915167+EvgeniyaSirotkina@users.noreply.github.com>
2020-01-13 03:04:30 +0300

    Update to_csv.rb

--- to_csv.rb
+++ to_csv.rb
@@ -3,13 +3,7 @@ require 'csv'
 class ToCsv
 
    def initialize file_name
-	# file name validation
 	@file_name = file_name.slice(/.csv/).nil? ? file_name + ".csv" : file_name
-	#if file_name.slice(/.csv/).nil?
-	#   @file_name = file_name + ".csv"
-	#else
-	#   @file_name = file_name
-	#end
    end
 
    def write_to_file products
